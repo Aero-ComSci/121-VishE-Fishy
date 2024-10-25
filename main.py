b3979b18dec15d051c757ed0ec38487c26d11b57
VishE-Fishy <157423243+VishE-Fishy@users.noreply.github.com>
2024-10-24 23:44:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,58 @@
 import turtle as trtl
 import random as rand
 
-def Click():
-    def __init__ (self):
-        self.spot_color = "pink"
-        self.score = 0
-        self.timer_up = False
+
+class Click:
+    def __init__(self, colorlist, sizelist):
+        self.spotcolor = "pink"
         self.countdown_time = 30
+        self.colors = colorlist
+        self.sizes = sizelist
 
         self.wn = trtl.Screen()
+        self.wn.bgcolor("light blue")
 
-        self.spot = trtl.Turtle()
-        self.spot.shape("circle")
-        self.spot.shapesize(2)
-        self.spot.fillcolor(self.spot_color)
+        self.t = trtl.Turtle()
+        self.t.shape("circle")
+        self.t.shapesize(2)
+        self.t.fillcolor(self.spotcolor)
 
         self.counter = trtl.Turtle()
         self.counter.penup()
-        self.counter.goto(0, -260)  
+        self.counter.goto(0, -260)
         self.counter.hideturtle()
         self.counter.write(self.countdown_time, font=("Arial", 20, "normal"))
 
+        self.t.onclick(self.spotclick)
 
-        self.spot.onclick(self.spot_clicked)
+        self.wn.ontimer(self.countdown, 1000)
 
+        self.startgame()
 
-        self.wn.ontimer(self.countdown, 1000)  
-
-        self.wn.mainloop()
+    def startgame(self):
+         self.score = 0
+         self.timerup = False
+         self.wn.mainloop()
 
     def changepos(self):
-        self.t.goto(random.randit(-200,200),random.randit(-150,150) )
+        self.t.penup()
+        self.t.goto(rand.randint(-200, 200), rand.randint(-150, 150))
+        self.t.pendown()
+    def changecolor(self):
+        self.t.fillcolor(rand.choice(self.colors))
+        self.t.stamp()
+        self.t.fillcolor(self.spotcolor)
+    def sizechange(self):
+        self.t.shapesize(rand.choice(self.sizes))
     def update(self):
         self.score += 1
-        self.t.clear()
-        self.t.write(score, font=("Arial", 20, "normal"))
-        
+        self.t.write(self.score, font=("Arial", 20, "normal"))
 
     def spotclick(self, x, y):
-        if not timer_up:
+        if not self.timerup:
             self.changepos()
+            self.changecolor()
+            self.sizechange()
             self.update()
 
     def countdown(self):
@@ -47,12 +60,13 @@ def Click():
             self.countdown_time -= 1
             self.counter.clear()
             self.counter.write(self.countdown_time, font=("Arial", 20, "normal"))
-            self.wn.ontimer(self.countdown, 1000)  
+            self.wn.ontimer(self.countdown, 1000)
         else:
-            self.timer_up = True
-            self.spot.hideturtle()  
+            self.timerup = True
+            self.t.hideturtle()
             self.counter.clear()
             self.counter.write("Time's Up!", font=("Arial", 20, "normal"))
 
+
 if __name__ == "__main__":
-    game = Click()
+    game = Click(colorlist=["red","blue","black","purple", "green", "grey"], sizelist=[0.5,1,1.5,2,3.5,3,3.5,4])
